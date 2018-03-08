bf3f4c060ed765eb62f2cca4bd2edcd8f7b7678f
royabh <roy.abhriya@gmail.com>
2018-03-08 21:29:55 +0530

    app bar behavour and resources added

---
+++ app/src/main/java/zebrostudio/wallr100/utils/DisableAppBarLayoutBehaviour.java
@@ -0,0 +1,32 @@
+package zebrostudio.wallr100.utils;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DisableAppBarLayoutBehaviour extends AppBarLayout.Behavior {
+    private boolean mEnabled = true;
+
+    public DisableAppBarLayoutBehaviour() {
+        super();
+    }
+
+    public DisableAppBarLayoutBehaviour(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setEnabled(boolean enabled) {
+        mEnabled = enabled;
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes) {
+        return mEnabled && super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    public boolean isEnabled() {
+        return mEnabled;
+    }
+}
