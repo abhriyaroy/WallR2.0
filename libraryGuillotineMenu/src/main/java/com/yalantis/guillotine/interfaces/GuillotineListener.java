efd22f6c7243ecd76dfacc8e056decae58bc2938
royabh <roy.abhriya@gmail.com>
2018-03-22 15:53:15 +0530

    Reformat lines according to standards
    All extra line breaks and spacing throughout the project have been
    removed.

--- libraryGuillotineMenu/src/main/java/com/yalantis/guillotine/interfaces/GuillotineListener.java
+++ libraryGuillotineMenu/src/main/java/com/yalantis/guillotine/interfaces/GuillotineListener.java
@@ -1,9 +1,7 @@
 package com.yalantis.guillotine.interfaces;
 
-/**
- * Created by Dmytro Denysenko on 5/6/15.
- */
 public interface GuillotineListener {
-    void onGuillotineOpened();
-    void onGuillotineClosed();
+  void onGuillotineOpened();
+
+  void onGuillotineClosed();
 }
