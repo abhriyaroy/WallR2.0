dbd7c06d11e59482522ff0c58f89b557a1d1a70f
royabh <roy.abhriya@gmail.com>
2018-06-10 18:27:20 +0530

    Setup main activity

--- app/src/main/java/zebrostudio/wallr100/ui/main/MainActivity.kt
+++ app/src/main/java/zebrostudio/wallr100/ui/main/MainActivity.kt
@@ -1,13 +1,14 @@
 package zebrostudio.wallr100.ui.main
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import zebrostudio.wallr100.R
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+  var isGuillotineMenuOpen = false
+
+  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
