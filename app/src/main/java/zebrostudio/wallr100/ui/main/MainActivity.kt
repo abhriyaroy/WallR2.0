270c5a68d4dfc3a5a75563b3bae37cce040ff011
royabh <roy.abhriya@gmail.com>
2018-06-10 16:56:22 +0530

    Kotlin dependency added

---
+++ app/src/main/java/zebrostudio/wallr100/ui/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package zebrostudio.wallr100.ui.main
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import zebrostudio.wallr100.R
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+}
