7645ec90a7802bdb92f418d51e15cf93c5ed3846
royabh <roy.abhriya@gmail.com>
2018-06-14 07:25:35 +0530

    Reformat existing code

--- app/src/main/java/zebrostudio/wallr100/ui/main/MainActivity.kt
+++ app/src/main/java/zebrostudio/wallr100/ui/main/MainActivity.kt
@@ -2,22 +2,24 @@ package zebrostudio.wallr100.ui.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Pair
 import android.view.LayoutInflater
 import com.yalantis.guillotine.animation.GuillotineAnimation
 import com.yalantis.guillotine.interfaces.GuillotineListener
-import kotlinx.android.synthetic.main.activity_main.root_frame_layout
+import kotlinx.android.synthetic.main.activity_main.rootFrameLayout
 import kotlinx.android.synthetic.main.activity_main.toolbar
-import kotlinx.android.synthetic.main.guillotine_menu_layout.root_linear_layout_guillotine_menu
-import kotlinx.android.synthetic.main.item_guillotine_menu.view.imageview_guillotine_menu_item
-import kotlinx.android.synthetic.main.item_guillotine_menu.view.textview_guillotine_menu_item
-import kotlinx.android.synthetic.main.toolbar_layout.content_hamburger
+import kotlinx.android.synthetic.main.guillotine_menu_layout.rootLinearLayoutGuillotineMenu
+import kotlinx.android.synthetic.main.item_guillotine_menu.view.imageviewGuillotineMenuItem
+import kotlinx.android.synthetic.main.item_guillotine_menu.view.textviewGuillotineMenuItem
+import kotlinx.android.synthetic.main.toolbar_layout.contentHamburger
 import zebrostudio.wallr100.R
+import zebrostudio.wallr100.utils.colorRes
+import zebrostudio.wallr100.utils.drawableRes
+import zebrostudio.wallr100.utils.stringRes
 
 class MainActivity : AppCompatActivity() {
 
   private var isGuillotineMenuOpen = false
-  private var guillotineMenuAnimation: GuillotineAnimation? = null
+  private lateinit var guillotineMenuAnimation: GuillotineAnimation
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -28,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
   override fun onBackPressed() {
     if (isGuillotineMenuOpen) {
-      guillotineMenuAnimation?.close()
+      guillotineMenuAnimation.close()
     } else {
       super.onBackPressed()
     }
@@ -37,7 +39,7 @@ class MainActivity : AppCompatActivity() {
   private fun initializeViews() {
     val guillotineMenu = LayoutInflater.from(this)
         .inflate(R.layout.guillotine_menu_layout, null)
-    root_frame_layout.addView(guillotineMenu)
+    rootFrameLayout.addView(guillotineMenu)
 
     setSupportActionBar(toolbar)
 
@@ -54,8 +56,8 @@ class MainActivity : AppCompatActivity() {
     val rippleDuration = 250
     guillotineMenuAnimation = GuillotineAnimation.GuillotineBuilder(
         guillotineMenu,
-        guillotineMenu.findViewById(R.id.hamburger_guillotine_menu),
-        content_hamburger)
+        guillotineMenu.findViewById(R.id.hamburgerGuillotineMenu),
+        contentHamburger)
         .setStartDelay(rippleDuration.toLong())
         .setActionBarViewForAnimation(toolbar)
         .setGuillotineListener(guillotineListener)
@@ -68,40 +70,34 @@ class MainActivity : AppCompatActivity() {
   private fun buildGuillotineMenuItems(): List<Pair<Int, Int>> {
     // Declare mutable list containing names and icon resources of guillotine menu items
     val menuItemDetails = mutableListOf<Pair<Int, Int>>()
-    menuItemDetails.add(
-        Pair(R.string.guillotine_explore_title, R.drawable.ic_explore_white))
-    menuItemDetails.add(
-        Pair(R.string.guillotine_toppicks_title, R.drawable.ic_toppicks_white))
-    menuItemDetails.add(
-        Pair(R.string.guillotine_categories_title, R.drawable.ic_categories_white))
-    menuItemDetails.add(
-        Pair(R.string.guillotine_minimal_title, R.drawable.ic_minimal_white))
-    menuItemDetails.add(
-        Pair(R.string.guillotine_collection_title, R.drawable.ic_collections_white))
-    menuItemDetails.add(
-        Pair(R.string.guillotine_feedback_title, R.drawable.ic_feedback_white))
-    menuItemDetails.add(
-        Pair(R.string.guillotine_buypro_title, R.drawable.ic_buypro_black))
+    menuItemDetails.add(R.string.guillotine_explore_title to R.drawable.ic_explore_white)
+    menuItemDetails.add(R.string.guillotine_toppicks_title to R.drawable.ic_toppicks_white)
+    menuItemDetails.add(R.string.guillotine_categories_title to R.drawable.ic_categories_white)
+    menuItemDetails.add(R.string.guillotine_minimal_title to R.drawable.ic_minimal_white)
+    menuItemDetails.add(R.string.guillotine_collection_title to R.drawable.ic_collections_white)
+    menuItemDetails.add(R.string.guillotine_feedback_title to R.drawable.ic_feedback_white)
+    menuItemDetails.add(R.string.guillotine_buypro_title to R.drawable.ic_buypro_black)
     return menuItemDetails
   }
 
   private fun setUpGuillotineMenuItems(guillotineMenuItems: List<Pair<Int, Int>>) {
     // Programmatically add guillotine menu items
     val layoutInflater = LayoutInflater.from(this)
-    for (i in guillotineMenuItems.indices) {
+    val itemIterator = guillotineMenuItems.iterator()
+    itemIterator.forEach {
       val guillotineMenuItemView = layoutInflater
           .inflate(R.layout.item_guillotine_menu, null)
-      root_linear_layout_guillotine_menu?.addView(guillotineMenuItemView)
-      guillotineMenuItemView.id = guillotineMenuItems[i].first
-      guillotineMenuItemView.textview_guillotine_menu_item.text =
-          getString(guillotineMenuItems[i].first)
-      guillotineMenuItemView.imageview_guillotine_menu_item.setImageDrawable(
-          resources.getDrawable(guillotineMenuItems[i].second))
+      rootLinearLayoutGuillotineMenu?.addView(guillotineMenuItemView)
+      guillotineMenuItemView.id = it.first
+      guillotineMenuItemView.textviewGuillotineMenuItem.text =
+          this.stringRes(it.first)
+      guillotineMenuItemView.imageviewGuillotineMenuItem.setImageDrawable(
+          this.drawableRes(it.second))
       // Make the background white and text color black for the buy pro guillotine menu item
-      if (i == guillotineMenuItems.size - 1) {
-        guillotineMenuItemView.setBackgroundColor(resources.getColor(R.color.color_white))
-        guillotineMenuItemView.textview_guillotine_menu_item
-            .setTextColor(resources.getColor(R.color.color_black))
+      if (!itemIterator.hasNext()) {
+        guillotineMenuItemView.setBackgroundColor(this.colorRes(R.color.color_white))
+        guillotineMenuItemView.textviewGuillotineMenuItem
+            .setTextColor(this.colorRes(R.color.color_black))
       }
     }
   }
