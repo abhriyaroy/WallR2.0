15b5a5337b86aa58eb51fb39bc2146c3e6cb9aa4
royabh <roy.abhriya@gmail.com>
2018-06-18 22:37:11 +0530

    Update fragment name in data repository

--- app/src/main/java/zebrostudio/wallr100/ui/explore/ExploreFragment.kt
+++ app/src/main/java/zebrostudio/wallr100/ui/explore/ExploreFragment.kt
@@ -3,13 +3,17 @@ package zebrostudio.wallr100.ui.explore
 import zebrostudio.wallr100.R
 import zebrostudio.wallr100.ui.basefragment.BaseFragment
 import zebrostudio.wallr100.utils.stringRes
+import javax.inject.Inject
 
 class ExploreFragment : BaseFragment(){
+
+  @Inject
+  internal lateinit var presenter: ExplorePresenterImpl
 
   val FRAGMENTNAME = activity?.stringRes(R.string.guillotine_explore_title)
 
   override fun onResume() {
     super.onResume()
-    
+    presenter.updateFragmentName()
   }
 }
--- app/src/main/java/zebrostudio/wallr100/ui/explore/ExplorePresenterImpl.kt
+++ app/src/main/java/zebrostudio/wallr100/ui/explore/ExplorePresenterImpl.kt
@@ -5,6 +5,8 @@ import zebrostudio.wallr100.data.DataRepository
 class ExplorePresenterImpl
 constructor(dataRepository: DataRepository) : ExploreContract.ExplorePresenter {
 
+  private val EXPLORE_FRAGMENT_TAG = "Explore"
+  private var dataRepository = dataRepository
   private var exploreView: ExploreContract.ExploreView? = null
 
   override fun attachView(view: ExploreContract.ExploreView) {
@@ -16,7 +18,7 @@ constructor(dataRepository: DataRepository) : ExploreContract.ExplorePresenter {
   }
 
   override fun updateFragmentName() {
-
+    dataRepository.updateCurrentFragmentName(EXPLORE_FRAGMENT_TAG)
   }
 
 }
