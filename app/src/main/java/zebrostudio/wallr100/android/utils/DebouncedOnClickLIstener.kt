7f9f14eddaa3bed0722b6b4fd4de8f6dd75392ab
royabh <roy.abhriya@gmail.com>
2018-07-23 20:52:56 +0530

    Refactor line indents

---
+++ app/src/main/java/zebrostudio/wallr100/android/utils/DebouncedOnClickLIstener.kt
@@ -0,0 +1,28 @@
+package zebrostudio.wallr100.android.utils
+
+import android.os.SystemClock
+import android.view.View
+import java.util.WeakHashMap
+
+abstract class DebouncedOnClickListener(private val minimumInterval: Long = 1000) :
+    View.OnClickListener {
+
+  private val lastClickMap: MutableMap<View, Long>
+
+  abstract fun onDebouncedClick(v: View)
+
+  init {
+    this.lastClickMap = WeakHashMap<View, Long>()
+  }
+
+  override fun onClick(clickedView: View) {
+    val previousClickTimestamp = lastClickMap[clickedView]
+    val currentTimestamp = SystemClock.uptimeMillis()
+
+    lastClickMap[clickedView] = currentTimestamp
+    if (previousClickTimestamp == null ||
+        currentTimestamp - previousClickTimestamp.toLong() > minimumInterval) {
+      onDebouncedClick(clickedView)
+    }
+  }
+}
