6b081089c5282e94e640941aca9d3509a75563e3
royabh <roy.abhriya@gmail.com>
2018-11-11 18:12:23 +0530

    Initial commit

---
+++ app/src/main/java/zebrostudio/wallr100/android/detail/DetailActivity.kt
@@ -0,0 +1,13 @@
+package zebrostudio.wallr100.android.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import zebrostudio.wallr100.R
+
+class DetailActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_detail)
+  }
+}
