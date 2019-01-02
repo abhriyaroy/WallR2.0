daabc86bb1935c1d069e5d08105ec6a2605eda3b
royabh <roy.abhriya@gmail.com>
2019-01-02 11:04:22 +0530

    Add pending transaction to base activity'

--- app/src/main/java/zebrostudio/wallr100/android/ui/BaseActivity.kt
+++ app/src/main/java/zebrostudio/wallr100/android/ui/BaseActivity.kt
@@ -1,15 +1,27 @@
 package zebrostudio.wallr100.android.ui
 
 import android.arch.lifecycle.Lifecycle
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.uber.autodispose.ScopeProvider
 import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
+import zebrostudio.wallr100.R
 import zebrostudio.wallr100.presentation.BaseView
 
 abstract class BaseActivity : AppCompatActivity(), BaseView {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    overridePendingTransition(R.anim.slide_from_right, R.anim.no_change)
+  }
+
   override fun getScope(): ScopeProvider {
     return AndroidLifecycleScopeProvider.from(this, Lifecycle.Event.ON_DESTROY)
+  }
+
+  override fun onBackPressed() {
+    super.onBackPressed()
+    overridePendingTransition(R.anim.no_change, R.anim.slide_to_right)
   }
 
 }
