9792020a8b7cd510d1277211fbe2d12bb421ace6
royabh <roy.abhriya@gmail.com>
2019-01-17 12:12:37 +0530

    Add uCrop dependency

---
+++ libraryUCrop/src/main/java/com/yalantis/ucrop/callback/OverlayViewChangeListener.java
@@ -0,0 +1,11 @@
+package com.yalantis.ucrop.callback;
+
+import android.graphics.RectF;
+
+/**
+ * Created by Oleksii Shliama.
+ */
+public interface OverlayViewChangeListener {
+
+  void onCropRectUpdated(RectF cropRect);
+}
