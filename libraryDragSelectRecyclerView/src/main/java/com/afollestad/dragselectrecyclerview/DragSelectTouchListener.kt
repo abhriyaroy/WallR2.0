d18dd5898e301a5ae1fb7cc7bdcf5ac6502cc485
royabh <roy.abhriya@gmail.com>
2019-03-24 17:55:36 +0530

    Remove redundant lines from drag select library

--- libraryDragSelectRecyclerView/src/main/java/com/afollestad/dragselectrecyclerview/DragSelectTouchListener.kt
+++ libraryDragSelectRecyclerView/src/main/java/com/afollestad/dragselectrecyclerview/DragSelectTouchListener.kt
@@ -1,18 +1,3 @@
-/**
- * Designed and developed by Aidan Follestad (@afollestad)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 @file:Suppress("MemberVisibilityCanBePrivate", "unused")
 
 package com.afollestad.dragselectrecyclerview
@@ -37,7 +22,6 @@ enum class Mode {
 
 typealias AutoScrollListener = (scrolling: Boolean) -> Unit
 
-/** @author Aidan Follestad (afollestad) */
 class DragSelectTouchListener private constructor(
   context: Context,
   private val receiver: DragSelectReceiver
