3cbb775eced99ef0ee3fc3aaaf1bf02e900a4699
royabh <roy.abhriya@gmail.com>
2019-05-07 08:49:33 +0530

    Configure collections item presenter

--- app/src/main/java/zebrostudio/wallr100/presentation/adapters/CollectionRecyclerPresenterImpl.kt
+++ app/src/main/java/zebrostudio/wallr100/presentation/adapters/CollectionRecyclerPresenterImpl.kt
@@ -1,19 +1,30 @@
 package zebrostudio.wallr100.presentation.adapters
 
 import zebrostudio.wallr100.presentation.adapters.CollectionRecyclerContract.CollectionRecyclerPresenter
+import zebrostudio.wallr100.presentation.adapters.CollectionRecyclerContract.CollectioneRecyclerItemViewHolder
+import zebrostudio.wallr100.presentation.minimal.INITIAL_OFFSET
+import zebrostudio.wallr100.presentation.minimal.INITIAL_SIZE
 
 class CollectionRecyclerPresenterImpl : CollectionRecyclerPresenter {
-  override fun getItemCount(list: List<String>): Int {
 
+  override fun getItemCount(list: List<String>): Int {
+    return list.size
   }
 
   override fun onBindRepositoryRowViewAtPosition(
-    dragSelectItemViewHolder: CollectionRecyclerContract.CollectioneRecyclerItemViewHolder,
+    dragSelectItemViewHolder: CollectioneRecyclerItemViewHolder,
     imagePathList: List<String>,
     selectedItemsMap: HashMap<Int, String>,
     position: Int
   ) {
-
+    dragSelectItemViewHolder.setImage(imagePathList[position])
+    dragSelectItemViewHolder.attachClickListener()
+    dragSelectItemViewHolder.attachLongClickListener()
+    if (selectedItemsMap.size != INITIAL_SIZE) {
+      if (selectedItemsMap.containsKey(position - INITIAL_OFFSET)) {
+        dragSelectItemViewHolder.showSelectedIndicator()
+      }
+    }
   }
 
 }
