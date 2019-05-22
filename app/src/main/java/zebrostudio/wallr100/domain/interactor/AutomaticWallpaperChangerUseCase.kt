53504c17f55449b78ea60af44403b211da4e6456
abhriyaroy <roy.abhriya@gmail.com>
2019-05-22 23:42:47 +0530

    Use rx java timer with repeat for foreground service and lof in file

--- app/src/main/java/zebrostudio/wallr100/domain/interactor/AutomaticWallpaperChangerUseCase.kt
+++ app/src/main/java/zebrostudio/wallr100/domain/interactor/AutomaticWallpaperChangerUseCase.kt
@@ -1,6 +1,8 @@
 package zebrostudio.wallr100.domain.interactor
 
 import android.graphics.Bitmap
+import android.os.Environment
+import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.disposables.Disposable
 import zebrostudio.wallr100.android.service.AutomaticWallpaperChangerService
@@ -8,16 +10,25 @@ import zebrostudio.wallr100.android.service.WALLPAPER_CHANGER_INTERVALS_LIST
 import zebrostudio.wallr100.android.utils.WallpaperSetter
 import zebrostudio.wallr100.domain.WallrRepository
 import zebrostudio.wallr100.domain.executor.PostExecutionThread
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 interface AutomaticWallpaperChangerUseCase {
   fun attachService(automaticWallpaperChangerService: AutomaticWallpaperChangerService)
   fun detachService()
-  fun handleRunnableCall()
+  fun handleServiceStarted()
+  fun handleServiceDestroyed()
   fun getInterval(): Long
 }
 
 const val INDEX_OF_FIRST_ELEMENT_IN_LIST = 0
 const val INDEX_UNDERFLOW = -1
+const val TIME_CHECKER_INTERVAL: Long = 300000
 
 class AutomaticWallpaperChangerInteractor(
   private val wallpaperSetter: WallpaperSetter,
@@ -26,7 +37,8 @@ class AutomaticWallpaperChangerInteractor(
 ) : AutomaticWallpaperChangerUseCase {
 
   internal var lastWallpaperChangeTime: Long = System.currentTimeMillis()
-  private var disposable: Disposable? = null
+  private var timerDisposable: Disposable? = null
+  private var wallpaperChangerDisposable: Disposable? = null
   private var automaticWallpaperChangerService: AutomaticWallpaperChangerService? = null
 
   override fun attachService(automaticWallpaperChangerService: AutomaticWallpaperChangerService) {
@@ -37,11 +49,25 @@ class AutomaticWallpaperChangerInteractor(
     automaticWallpaperChangerService = null
   }
 
-  override fun handleRunnableCall() {
-    System.currentTimeMillis().let {
-      if (it - lastWallpaperChangeTime >= getInterval()) {
-        changeWallpaper()
-      }
+  override fun handleServiceStarted() {
+    timerDisposable = Observable.timer(TIME_CHECKER_INTERVAL, MILLISECONDS)
+        .repeat()
+        .doOnNext {
+          appendLog("on next in timer disposable called")
+          if (System.currentTimeMillis() - lastWallpaperChangeTime >= getInterval()) {
+            changeWallpaper()
+          }
+        }
+        .doOnError {
+          appendLog("on error in timer disposable called ${it.message}")
+        }
+        .subscribe()
+  }
+
+  override fun handleServiceDestroyed() {
+    appendLog("on handle service destroyed called")
+    if (timerDisposable?.isDisposed != true) {
+      timerDisposable?.dispose()
     }
   }
 
@@ -56,21 +82,22 @@ class AutomaticWallpaperChangerInteractor(
   }
 
   private fun changeWallpaper() {
-    if (disposable?.isDisposed == false) {
-      disposable?.dispose()
-    }
-    disposable = getWallpaperBitmap()
-        .doOnSuccess {
+    wallpaperChangerDisposable = getWallpaperBitmap().toObservable()
+        .delay(getInterval(), MILLISECONDS)
+        .repeat()
+        .doOnNext {
+
+          appendLog("wallpaper changed ")
           wallpaperSetter.setWallpaper(it)
+          lastWallpaperChangeTime = System.currentTimeMillis()
           if (!it.isRecycled) {
             it.recycle()
           }
         }.observeOn(postExecutionThread.scheduler)
-        .subscribe({
-          lastWallpaperChangeTime = System.currentTimeMillis()
-        }, {
+        .doOnError {
           automaticWallpaperChangerService?.stopService()
-        })
+        }
+        .subscribe()
   }
 
   private fun getWallpaperBitmap(): Single<Bitmap> {
@@ -93,5 +120,37 @@ class AutomaticWallpaperChangerInteractor(
             wallrRepository.getBitmapFromDatabaseImage(it)
           }
         }
+  }
+
+  companion object {
+    fun appendLog(text: String) {
+      val logFile =
+          File(Environment.getExternalStorageDirectory().path + File.separator + "wallrlog.txt")
+      if (!logFile.exists()) {
+        try {
+          logFile.createNewFile()
+        } catch (e: IOException) {
+          // TODO Auto-generated catch block
+          e.printStackTrace()
+        }
+
+      }
+      try {
+        val currentDate = Date(System.currentTimeMillis())
+
+        val df = SimpleDateFormat("dd:MM:yy:HH:mm:ss")
+
+        val newText = "$text on ${df.format(currentDate)}"
+
+        //BufferedWriter for performance, true to set append to file flag
+        val buf = BufferedWriter(FileWriter(logFile, true))
+        buf.append(newText)
+        buf.newLine()
+        buf.close()
+      } catch (e: IOException) {
+        // TODO Auto-generated catch block
+        e.printStackTrace()
+      }
+    }
   }
 }
