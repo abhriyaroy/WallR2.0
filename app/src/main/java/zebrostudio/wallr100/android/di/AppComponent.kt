d9869c4e75017f07451c9b503e0e8de956896937
Abhriya Roy <roy.abhriya@gmail.com>
2019-06-19 15:18:52 +0530

    Add Android injector

--- app/src/main/java/zebrostudio/wallr100/android/di/AppComponent.kt
+++ app/src/main/java/zebrostudio/wallr100/android/di/AppComponent.kt
@@ -4,14 +4,14 @@ import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
-import dagger.android.support.AndroidSupportInjectionModule
 import zebrostudio.wallr100.android.WallrApplication
-import zebrostudio.wallr100.android.di.scopes.PerApplication
 import javax.inject.Singleton
 
-@PerApplication
-@Component(modules = [(AndroidSupportInjectionModule::class), (AppModule::class),
-  (ActivityBuilder::class), (ServiceBuilder::class)])
+@Singleton
+@Component(
+    modules = [(AndroidInjectionModule::class), (AppModule::class),
+      (ActivityBuilder::class), (ServiceBuilder::class)]
+)
 interface AppComponent {
 
   @Component.Builder
